a4deca262a618030b02863b0a1a9f724e51cb146
TammyLe175 <ngochongle19@gmail.com>
2023-04-20 21:07:48 -0500

    added first file

---
+++ account.py
@@ -0,0 +1,51 @@
+
+class Account:
+    def __init__(self, name: str) -> None:
+        """
+        function to set up the account
+        :param name: account name
+
+        """
+        balance = 0
+        self.__account_name = name
+        self.__account_balance = balance
+
+
+    def deposit(self, amount: float) -> bool:
+        """
+        function deposit a amount of money, cannot be less than or equal to 0
+        :param amount: ammount money deposit in account
+
+        """
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount: float) -> bool:
+        """
+
+
+        :param amount: the amount of money to withdraw in the account
+        :return: an action of withdraw an amount of money
+        """
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+    def get_balance(self):
+        """
+
+        :return: the current balance in the account
+        """
+        return self.__account_balance
+    def get_name(self):
+        """
+
+        :return: the name of the account
+        """
+        return self.__account_name
+
+
